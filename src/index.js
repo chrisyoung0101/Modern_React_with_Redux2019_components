3f7fa2bad7ac95eabaa994a6d5795101638bf68e
Chris Young <chrisyoung0101@gmail.com>
2020-01-06 20:49:38 -0600

    Adding more props.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//import faker from 'faker';
+import faker from 'faker';
 // ./ says look in the folder we are in.
 // webpack looks for all the js files named CommentDetail
-import CommentDetail from './CommentDetail'; 
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Gigi" />
-            <CommentDetail author="Wimpy" />
-            <CommentDetail author="Donut" />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                commentText="This is wow." 
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Alex" 
+                timeAgo="Today at 1:00AM" 
+                commentText="Like crazy." 
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Gigi" 
+                timeAgo="Yesterday at 8:00AM" 
+                commentText="What?! OMG." 
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Wimpy" 
+                timeAgo="Today at 2:30PM" 
+                commentText="Really Dude?" 
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Donut" 
+                timeAgo="Yesterday at 10:00AM" 
+                commentText="What tha?! Ha Ha HA." 
+            />
         </div>
     );
 };
